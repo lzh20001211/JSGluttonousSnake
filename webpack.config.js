4188838a8274cec5f4b8dd18a069af830b4cf228
liuzihan2019302110454 <986689129@qq.com>
2021-11-14 19:01:10 +0800

    git test

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+module.exports = {
+
+    entry: "./src/index.ts",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js"
+    },
+    module: {
+        rules:[
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude:/node_modules/
+            },
+
+            {
+                test: /\.less$/,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "less-loader"
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin(
+            { 
+                template:"./src/index.html"
+            }
+        ),
+    ],
+
+
+    resolve: {
+        extensions: ['.ts','.js']
+    }
+
+}
